4abfdbcfd874bc4619b4581303ae69132b62f06c
Osaid Basha <151678643+Osaid-Basha@users.noreply.github.com>
2025-08-04 11:48:55 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (58.33%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -6,11 +6,11 @@ public:
             if(candies[i]>maxValue)
                 maxValue=candies[i];
         }
-        int size =candies.size();
+        
         
        vector<bool> result1;
        
-        for(int i=0;i<size;i++)
+        for(int i=0;i<candies.size();i++)
         {
             if( candies[i]+extraCandies <maxValue){
                 result1.push_back(false);
