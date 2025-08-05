57fdb82f8a2d9efaf0300e5d4b1c1edc2bcf06aa
Osaid Basha <151678643+Osaid-Basha@users.noreply.github.com>
2025-08-06 01:08:43 +0300

    Time: 0 ms (100.00%) | Memory: 10.9 MB (8.78%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string y= to_string(x);
+        string z;
+        for(int i=y.length()-1;i>=0;i--){
+            z+=y[i];
+        }
+        if(y==z){
+            return true;
+        }
+     
+        return false;
+    }
+};
